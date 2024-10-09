7d885d97fa9e38d5fb7db6acfd5807c6075e7ec3
kmw5531 <139738859+kmw5531@users.noreply.github.com>
2024-10-09 15:56:02 +0900

    이[Feat] 이메일 중복 확인절차 구현

--- src/main/java/com/project/capstone/member/repository/MemberRepository.java
+++ src/main/java/com/project/capstone/member/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package com.project.capstone.member.repository;
 
 import com.project.capstone.member.domain.Member;
+import jakarta.validation.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -13,4 +15,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email); // 이메일로 멤버 아이디 조회
 
+    Boolean existsByEmail(String email);
 }
